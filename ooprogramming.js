806c7d64a103594418345c65f5ba741f4efe945b
Oladimeji Abdrahman Adebayo <67334984+rahmlad-aramide@users.noreply.github.com>
2022-07-12 12:02:35 +0000

    Initial commit

---
+++ ooprogramming.js
@@ -0,0 +1,23 @@
+// Task 1: Code a Person class
+class Person {
+    constructor(name = "Tom", age = "20", energy = "100") {
+        this.name = name;
+        this.age = age;
+        this.energy = energy;
+        
+    }
+    sleep() {
+        thisenergy += 10;
+    }
+    doSomethingFun(energy) {
+        energy -= 10;
+    }
+}
+// Task 2: Code a Worker class
+
+
+// Task 3: Code an intern object
+
+// Task 4: Code a manager object
+
+// Task 5: Run methods on object instances
